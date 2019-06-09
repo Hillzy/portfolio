e129f077ae4a331a62d44a7cdfd66caaf0a9c898
Hillzy <hillierliam37@gmail.com>
2019-06-09 15:25:39 +1000

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -342,6 +342,35 @@ const Contact = () => {
           <Tags tag="h1" type="open" indent={0} />
           <h1>Contact Me</h1>
           <Tags tag="h1" type="close" indent={0} />
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{" "}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </div>
         <Tags tag="body" type="close" indent={0} />
         <Tags tag="html" type="close" indent={-1} />
